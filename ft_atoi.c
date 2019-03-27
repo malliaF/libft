b90e6c4833ae3e055de18f3a0774655809a1ccc3
Masha Fedotova <malliafedotova@gmail.com>
2019-03-27 17:50:38 +0300

    27.03.2019

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	long	result;
+	long	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' ||
+			str[i] == '\v' || str[i] == '\n' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int)(result * sign));
+}
--- libft.h
+++ libft.h
@@ -37,4 +37,8 @@ char	*ft_strstr(const char *str, const char *substr);
 char	*ft_strnstr(const char *str, const char *substr, size_t n);
 int	ft_strcmp(const char *str1, const char *str2);
 int	ft_strncmp(const char *str1, const char *str2, size_t n);
+int	ft_atoi(const char *str);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 #endif
